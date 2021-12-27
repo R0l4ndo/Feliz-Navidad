8fd0feeab05704ac21e64e8e65951e51746f921b
kuenta264 <kuenta264@gmail.com>
2021-12-26 20:21:34 -0500

    5

---
+++ app.js
@@ -0,0 +1,17 @@
+const formulario = document.getElementById('form-Inicio');
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('https://sheet.best/api/sheets/341aee14-b767-459d-816c-d9aff760687a', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Nombre": formulario.Nombre.value,
+            "Apellido": formulario.Apellido.value
+        })
+    });
+});
