8c4fe09efd3bf4836e9741e45d2048f96d51f146
kuenta264 <kuenta264@gmail.com>
2021-12-27 20:20:18 -0500

    8

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-const formulario = document.getElementById('form-Inicio');
-formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    fetch('', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "Nombre": "hola"
-        })
-    });
-    
-});
 function validar() {
     var user = document.getElementById('Nombre').value;
     var user2 = document.getElementById('Apellido').value;
