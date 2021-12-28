0c45ca702fafc3cc783faf53365574a26dde9eb9
kuenta264 <kuenta264@gmail.com>
2021-12-27 19:07:01 -0500

    5

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const formulario = document.getElementById('form-Inicio');
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
-
     fetch('', {
         method: 'POST',
         mode: 'cors',
@@ -15,33 +14,12 @@ formulario.addEventListener('submit', (e) => {
     });
     
 });
-
-
 function validar() {
     var user = document.getElementById('Nombre').value;
     var user2 = document.getElementById('Apellido').value;
-    const sonido = document.getElementById('sonido');
     if (user == "Rolan" && user2=="Alburqueque") {
-        sonido.innerHTML = '<audio src="Ese no se qué.mp3" controls></audio>';
-        const { value: linea1 } = await Swal.fire({
-            title: "Cancion",
-            text: "Primera linea",
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            confirmButtonText: "Siguiente",
-        });
-        if (linea1) {
-            const { value: linea2} = await Swal.fire({
-            title: "Esta es una indirecta?",
-            text: "Prueba2",
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            confirmButtonText: 'Siguiente',
-        });
-        
-        }
-
-    } else {
-        alert("ingreso fallido");
+        location.href = 'mensaje.html';
     }
-}
+    } 
+
+            
